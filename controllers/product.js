9f1892e7382878beffb1820e8e67048ba66b4f2d
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-29 23:51:24 +0530

    add-controller

---
+++ controllers/product.js
@@ -0,0 +1,43 @@
+exports.getAllProducts = (req, res) => {
+  res.json(data.products);
+};
+
+exports.getProductsWithId = (req, res) => {
+  const id = +req.params.id;
+  // res.json(data.products[id - 1]);
+  // OR
+  const products = data.products.find((product) => product.id === id);
+  res.json(products);
+};
+
+exports.createProduct = (req, res) => {
+  data.products.push(req.body);
+  res.json(req.body);
+};
+
+exports.deleteProduct = (req, res) => {
+  const id = +req.params.id;
+  const products = data.products.filter((product) => product.id !== id);
+  res.json(products);
+};
+
+exports.patchProduct = (req, res) => {
+  const id = +req.params.id;
+  const productIndex = data.products.findIndex((product) => product.id === id);
+  data.products.splice(productIndex, 1, { id: id, ...req.body });
+  res.json(req.body);
+};
+
+exports.updateProduct = (req, res) => {
+  (req, res) => {
+    const id = +req.params.id;
+    const productIndex = data.products.findIndex(
+      (product) => product.id === id
+    );
+    data.products.splice(productIndex, 1, {
+      ...data.products[productIndex],
+      ...req.body,
+    });
+    res.json(req.body);
+  };
+};
