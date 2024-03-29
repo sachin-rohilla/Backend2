9ebbe9a63ba3d41adfa6bc28d66e7fb5d9d8c147
sachin-rohilla <sachinrohilla23799@gmail.com>
2024-03-29 23:58:30 +0530

    fix-code

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 const productRoutes = require("./routes/product");
+
 // middleware
 app.use(express.json());
 app.use("/", productRoutes.router);
 
+// default routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
